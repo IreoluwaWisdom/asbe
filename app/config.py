8f02f6b9452ce2488a80ee343d73c9ae0d4870e5
OsazuwaEmmanuel <osazuwaemmanuel25@gmail.com>
2025-02-17 00:20:46 +0100

    Trying to set everything to string.

--- app/config.py
+++ app/config.py
@@ -5,16 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Fetch database credentials
-DB_HOST = os.getenv('AS_DB_HOST')
-DB_NAME = os.getenv('AS_DB_NAME')
-DB_USER = os.getenv('AS_DB_USER')
-DB_PASSWORD = os.getenv('AS_DB_PASSWORD')
+DB_HOST = str(os.getenv('AS_DB_HOST'))
+DB_NAME = str(os.getenv('AS_DB_NAME'))
+DB_USER = str(os.getenv('AS_DB_USER'))
+DB_PASSWORD = str(os.getenv('AS_DB_PASSWORD'))
 
 # Email settings
-MAIL_USERNAME = os.getenv("AS_MAIL_USERNAME")
-MAIL_PASSWORD = os.getenv("AS_MAIL_PASSWORD")
-MAIL_FROM = os.getenv("AS_MAIL_FROM")
-MAIL_SERVER = os.getenv("AS_MAIL_SERVER")
-MAIL_PORT = int(os.getenv("AS_MAIL_PORT", 587))
-MAIL_STARTTLS = os.getenv("AS_MAIL_STARTTLS", "True").lower() == "true"
-MAIL_SSL_TLS = os.getenv("AS_MAIL_SSL_TLS", "False").lower() == "true"
+MAIL_USERNAME = str(os.getenv("AS_MAIL_USERNAME"))
+MAIL_PASSWORD = str(os.getenv("AS_MAIL_PASSWORD"))
+MAIL_FROM = str(os.getenv("AS_MAIL_FROM"))
+MAIL_SERVER = str(os.getenv("AS_MAIL_SERVER"))
+MAIL_PORT = int(str(os.getenv("AS_MAIL_PORT", "587")))
+MAIL_STARTTLS = str(os.getenv("AS_MAIL_STARTTLS", "True")).lower() == "true"
+MAIL_SSL_TLS = str(os.getenv("AS_MAIL_SSL_TLS", "False")).lower() == "true"
